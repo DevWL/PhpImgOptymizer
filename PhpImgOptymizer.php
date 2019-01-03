ac1cde6c461aed91af95104c239d2a28ab5f412c
DevWL <w.liszkiewicz@gmail.com>
2019-01-03 03:26:32 +0100

    *** no height constraints

--- PhpImgOptymizer.php
+++ PhpImgOptymizer.php
@@ -210,6 +210,7 @@ $options = [
     'compresionLvlForPng' => 9, // compresion strength from 0 to 9 where 9 is the strongest and 6 is the default
     'qualityForJpg' => 50, // image quality from 0 to 100 where 100 is the heighest quality and 75 is the default
     'maxWidth' => 1200, // images larger then 1200 will be resize to maxWidth value
+    // *** need to add height constraints as well
     'widthRange' => [850, 99999], // apply to images sized between 0px to 9000px width
     'optymizeFileLargerThen' => 50, // optymise files larger then number of Kb - example: 50 
     'fileNamePattern' => '%.*%', // compress only files which name mach this regular expresion 
